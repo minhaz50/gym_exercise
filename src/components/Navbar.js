07e3a98fc851ef67e2ff2a7abe929a974adf9348
minhaz2597 <md.minhazuddin2597@gmail.com>
2022-07-02 23:10:22 +0600

    Navbar file added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Stack } from "@mui/material";
+import "./Navbar.css";
+
+import Logo from "../assets/images/Logo.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Stack className="imgLogo">
+      <Link to="/">
+        <img
+          src={Logo}
+          alt="logo"
+          style={{ width: "48px", height: "48px", margin: "0 20px" }}
+        />
+      </Link>
+    </Stack>
+  );
 };
 
 export default Navbar;
